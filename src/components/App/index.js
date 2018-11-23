5f25eed15ccdbf53860eaaf03b50f4494fee6a0f
Anton Emmerik <anton@emmerik.se>
2018-11-23 16:15:30 +0700

    Basic setup with some css issues

---
+++ src/components/App/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends Component {
+  render() {
+    return <h1>project_title</h1>;
+  }
+}
+
+export default App;
